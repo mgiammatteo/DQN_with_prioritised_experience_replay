127f3b691e31ce6ed6ff468cae3b14f9f940efc2
MaxG <mgiammatteo@hotmail.com>
2019-01-18 07:13:23 +0000

    Commit working DQN with prioritised experience replay. Data structures such as SumTrees are not used for optimisation. However performance on LunarLander-v2 is still much better than Vanilla dqn

--- dqn_agent.py
+++ dqn_agent.py
@@ -15,7 +15,9 @@ GAMMA = 0.99            # discount factor
 TAU = 1e-3              # for soft update of target parameters
 LR = 5e-4               # learning rate 
 UPDATE_EVERY = 4        # how often to update the network
-E = 1e-4                # small constant to add to td_error
+E = 5e-2                # small constant to add to td_error
+ALPHA = 0.6             # PER hyperparameter 1
+BETA  = 0.6             # PER hyperparameter 2
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
@@ -65,6 +67,12 @@ class Agent():
         td_error = np.array(reward + GAMMA*max_q_next_state-expected)
         abs_td_error = torch.abs(torch.from_numpy(td_error))+E
         abs_td_error = abs_td_error.float().unsqueeze(0).to(device)
+       
+        # need to turn these back into numpy arrays before they get added to the replay buffer
+        # this is because the replay buffer will operate on them on the basis that they are numpy arrays
+        state = state.cpu().data.numpy()
+        next_state = next_state.cpu().data.numpy()
+        abs_td_error = abs_td_error.cpu().data.numpy()
         
         # Save experience in replay memory
         self.memory.add(state, action, reward, next_state, abs_td_error, done)
@@ -105,8 +113,8 @@ class Agent():
             experiences (Tuple[torch.Tensor]): tuple of (s, a, r, s', done) tuples 
             gamma (float): discount factor
         """
-        states, actions, rewards, next_states, abs_td_errors, dones = experiences
-
+        states, actions, rewards, next_states, abs_td_errors, dones, indices, weights = experiences
+       
         # Get max predicted Q values (for next states) from target model
         Q_targets_next = self.qnetwork_target(next_states).detach().max(1)[0].unsqueeze(1)
         # Compute Q targets for current states 
@@ -115,11 +123,18 @@ class Agent():
         # Get expected Q values from local model
         Q_expected = self.qnetwork_local(states).gather(1, actions)
 
+        # Latest abs TD-errors. They will be transformed into proper priorities in the ReplayBuffer class
+        priorities = torch.abs(Q_targets - Q_expected) + E
+        
         # Compute loss
-        loss = F.mse_loss(Q_expected, Q_targets)
+        loss = F.mse_loss(Q_expected*weights, Q_targets*weights)
         # Minimize the loss
         self.optimizer.zero_grad()
         loss.backward()
+        
+        # update the latest batch priorities in the replay buffer
+        self.memory.update_priorities(indices.data.cpu().numpy(), priorities.data.cpu().numpy())
+        
         self.optimizer.step()
 
         # ------------------- update target network ------------------- #
@@ -155,26 +170,51 @@ class ReplayBuffer:
         self.action_size = action_size
         self.memory = deque(maxlen=buffer_size)  
         self.batch_size = batch_size
-        self.experience = namedtuple("Experience", field_names=["state", "action", "reward", "next_state", "abs_td_error", "done"])
+        self.experience = namedtuple("Experience", field_names=["state", "action", "reward", "next_state", "priority", "done"])
         self.seed = random.seed(seed)
+        self.priorities = np.zeros((buffer_size,), dtype=np.float32)
+        self.pos = 0
+        self.buffer_size = buffer_size
     
-    def add(self, state, action, reward, next_state, abs_td_error, done):
+    def add(self, state, action, reward, next_state, priority, done):
         """Add a new experience to memory."""
-        e = self.experience(state, action, reward, next_state, abs_td_error, done)
+        e = self.experience(state, action, reward, next_state, priority, done)
         self.memory.append(e)
+        
+        self.priorities[self.pos] = priority
+        self.pos = (self.pos + 1) % self.buffer_size
+        
+    def update_priorities(self, batch_indices, batch_priorities):
+        for idx, prio in zip(batch_indices, batch_priorities):
+            self.priorities[idx] = prio
     
     def sample(self):
-        """Randomly sample a batch of experiences from memory."""
-        experiences = random.sample(self.memory, k=self.batch_size)
-     
+        """Sample a batch of experiences from memory based on their priority."""
+        if len(self.memory) == self.buffer_size:
+            prios = self.priorities
+        else:
+            prios = self.priorities[:self.pos]
+        
+        probs  = prios ** ALPHA
+        probs /= probs.sum()
+        
+        indices = np.random.choice(len(self.memory), self.batch_size, p=probs)
+        experiences = [self.memory[idx] for idx in indices]
+        
+        total    = len(self.memory)
+        weights  = (total * probs[indices]) ** (-BETA)
+        weights /= weights.max()
+        weights  = np.array(weights, dtype=np.float32)
+        
         states = torch.from_numpy(np.vstack([e.state for e in experiences if e is not None])).float().to(device)
         actions = torch.from_numpy(np.vstack([e.action for e in experiences if e is not None])).long().to(device)
         rewards = torch.from_numpy(np.vstack([e.reward for e in experiences if e is not None])).float().to(device)
         next_states = torch.from_numpy(np.vstack([e.next_state for e in experiences if e is not None])).float().to(device)
-        abs_td_errors = torch.from_numpy(np.vstack([e.abs_td_error for e in experiences if e is not None])).float().to(device)
+        priorities = torch.from_numpy(np.vstack([e.priority for e in experiences if e is not None])).float().to(device)
         dones = torch.from_numpy(np.vstack([e.done for e in experiences if e is not None]).astype(np.uint8)).float().to(device)
-        
-        return (states, actions, rewards, next_states, abs_td_error, dones)
+        indices = torch.from_numpy(np.vstack([index for index in indices]).astype(np.uint8)).int().to(device)
+        weights = torch.from_numpy(np.vstack([weight for weight in weights]).astype(np.float32)).float().to(device)
+        return (states, actions, rewards, next_states, priorities, dones, indices, weights)
 
     def __len__(self):
         """Return the current size of internal memory."""
